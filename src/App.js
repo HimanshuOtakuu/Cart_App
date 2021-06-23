a2e8ee2f094800afb08678ff41deee4403350930
HimanshuOtakuu <hs1925846@gmail.com>
2021-06-24 00:33:43 +0530

    firebase connected

--- src/App.js
+++ src/App.js
@@ -88,7 +88,10 @@ class App extends React.Component {
     let cartTotal = 0;
 
     products.map((product) => {
+      if(product.qty>0){
       cartTotal = cartTotal + product.qty * product.price
+      }
+      return '';
     })
 
     return cartTotal;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCaEPba6gIRtNlvobIn1snqL8Dph-ylIyc",
+  authDomain: "cartitem-a2d51.firebaseapp.com",
+  projectId: "cartitem-a2d51",
+  storageBucket: "cartitem-a2d51.appspot.com",
+  messagingSenderId: "738736079489",
+  appId: "1:738736079489:web:b16c9a748466ec2a5fc380"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +21,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
